7812891924c8fdbb474838994b679a25926d4893
Nanda Kishore <madhav.bnk@gmail.com>
2013-08-26 16:16:33 +0530

    Error handling in place

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -2,14 +2,12 @@ howfaryModule = angular.module('howfaryModule', []);
 
 howfaryModule.controller('formController', function($scope, $http){
     $scope.howfar = function(journey) {
-        console.log('clicked scope:'+journey.source);
         $http.post('/howfar/', {'source': $scope.journey.source, 'destination': $scope.journey.destination})
             .success(function(data, status) {
                 $scope.journey.duration = data.result.duration;
                 $scope.journey.distance = data.result.distance;
             }).error(function(data, status) {
-                console.log('Something Went Wrong');
-                console.log('error status:'+status);
+                $scope.error = 'Unable to Calculate Distance!';
             });
     };
 });
