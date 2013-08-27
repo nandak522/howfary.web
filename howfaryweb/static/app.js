0f9a42746b283919cefec4f072fdd67fe303da71
Nanda Kishore <madhav.bnk@gmail.com>
2013-08-27 09:35:46 +0530

    Updating new_journey details from DOM

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -1,11 +1,17 @@
 howfaryModule = angular.module('howfaryModule', []);
 
 howfaryModule.controller('formController', function($scope, $http){
-    $scope.howfar = function(journey) {
+    $scope.howfar = function() {
         $http.post('/howfar/', {'source': $scope.journey.source, 'destination': $scope.journey.destination})
             .success(function(data, status) {
                 $scope.journey.duration = data.result.duration;
                 $scope.journey.distance = data.result.distance;
+                new_journey_attrs = {'source': $scope.journey.source,
+                                     'destination': $scope.journey.destination,
+                                     'distance': data.result.distance,
+                                     'duration': data.result.duration}
+                $scope.new_journey = new_journey_attrs;
+                console.log('new_journey:' + $scope.new_journey);
             }).error(function(data, status) {
                 $scope.error = 'Unable to Calculate Distance!';
             });
