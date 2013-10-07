122fd942822e83e00a50a4c8b76dec8acbd59503
Nanda Kishore <jnandakishore@zeomega.com>
2013-10-07 19:20:36 +0530

    Removed directive as its not required

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('howfary', ['howfary.controllers', 'howfary.directives']);
+angular.module('howfary', ['howfary.controllers']);
--- howfaryweb/static/controllers.js
+++ howfaryweb/static/controllers.js
@@ -2,15 +2,17 @@
 
 angular.module('howfary.controllers', []).
     controller('formController', function($scope, $http){
+        $scope.newJournies = [];
         $scope.howfar = function() {
             $http.post('/howfar/', {'source': $scope.journey.source, 'destination': $scope.journey.destination})
                 .success(function(data, status) {
                     $scope.journey.duration = data.result.duration;
                     $scope.journey.distance = data.result.distance;
                     $scope.new_journey = {'source': $scope.journey.source,
-                                         'destination': $scope.journey.destination,
-                                         'distance': data.result.distance,
-                                         'duration': data.result.duration};
+                                          'destination': $scope.journey.destination,
+                                          'distance': data.result.distance,
+                                          'duration': data.result.duration};
+                    $scope.newJournies.unshift($scope.new_journey);
                 }).error(function(data, status) {
                     $scope.error = 'Unable to Calculate Distance!';
                 });
