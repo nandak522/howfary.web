990dd5cf3becebca8a90d2d4679ef72617164532
Nanda Kishore <jnandakishore@zeomega.com>
2013-11-07 14:44:57 +0530

    Final Cleanups

--- howfaryweb/static/controllers.js
+++ howfaryweb/static/controllers.js
@@ -3,7 +3,9 @@
 angular.module('howfary.controllers', []).
     controller('formController', function($scope, $http){
         $scope.newJournies = [];
+        $scope.submit_button_label = 'Calculate';
         $scope.howfar = function() {
+            $scope.submit_button_label = 'Calculating...';
             $http.post('/howfar/', {'source': $scope.journey.source, 'destination': $scope.journey.destination})
                 .success(function(data, status) {
                     $scope.journey.duration = data.result.duration;
@@ -14,6 +16,7 @@ angular.module('howfary.controllers', []).
                                           'duration': data.result.duration,
                                           'link': data.result.link};
                     $scope.newJournies.unshift($scope.new_journey);
+                $scope.submit_button_label = 'Calculate';
                 }).error(function(data, status) {
                     $scope.error = 'Unable to Calculate Distance!';
                 });
