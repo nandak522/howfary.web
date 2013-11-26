a574dff6f13e54a566b58260937232f84d89f6a8
Nanda Kishore <madhav.bnk@gmail.com>
2013-11-26 23:48:26 +0530

    Half baked directives.js code

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -1,3 +1,11 @@
-'use strict';
+"use strict";
 
-angular.module('howfary', ['howfary.controllers']);
+angular.module('howfary', ['howfary.controllers', 'howfary.directives']);
+
+
+// function initialize() {
+//   var sourceAutocomplete = new google.maps.places.Autocomplete(document.getElementById('source'));
+//   var destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'));
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize());
--- howfaryweb/static/controllers.js
+++ howfaryweb/static/controllers.js
@@ -1,12 +1,16 @@
-'use strict';
+(function () {
+   "use strict";
+   // this function is strict...
+}());
 
 angular.module('howfary.controllers', []).
-    controller('formController', function($scope, $http){
+    controller('formController', function ($scope, $http) {
         $scope.newJournies = [];
         $scope.submit_button_label = 'Calculate';
-        $scope.howfar = function() {
+        $scope.howfar = function () {
             $scope.submit_button_label = 'Calculating...';
             $scope.requestSent = true;
+            console.log("$scope.journey:" + $scope.journey);
             $http.post('/howfar/', {'source': $scope.journey.source, 'destination': $scope.journey.destination})
                 .success(function(data, status) {
                     $scope.journey.duration = data.result.duration;
@@ -17,8 +21,8 @@ angular.module('howfary.controllers', []).
                                           'duration': data.result.duration,
                                           'link': data.result.link};
                     $scope.newJournies.unshift($scope.new_journey);
-                $scope.submit_button_label = 'Calculate';
-                $scope.requestSent = false;
+                    $scope.submit_button_label = 'Calculate';
+                    $scope.requestSent = false;
                 }).error(function(data, status) {
                     $scope.error = 'Unable to Calculate Distance!';
                     $scope.requestSent = false;
