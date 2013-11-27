de9dceed3519e30aef6969a79283d7b84bcebd9a
Nanda Kishore <madhav.bnk@gmail.com>
2013-11-28 00:29:14 +0530

    Trying out directive

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -1,11 +1,3 @@
 "use strict";
 
 angular.module('howfary', ['howfary.controllers', 'howfary.directives']);
-
-
-// function initialize() {
-//   var sourceAutocomplete = new google.maps.places.Autocomplete(document.getElementById('source'));
-//   var destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'));
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize());
--- howfaryweb/static/directives.js
+++ howfaryweb/static/directives.js
@@ -1,30 +1,17 @@
-// angular.module('howfary.directives', [])
-// 	.directive('hfAddressAutcomplete', function () {
-//         return {
-//             restrict: 'E',
-//             replace: true,
-//             template: "<input ng-model='source' id='locationType' type='text' autocomplete='off' required/>",
-//             scope: {
-//                 locationType: '='
-//             },
-//             link: function($scope, element, attrs){
-//                 console.log('scope:'+ $scope);
-//                 console.log('element:'+ element);
-//                 console.log('attrs:'+ attrs);
-
-//                 var autocomplete = new google.maps.places.Autocomplete(element);
-//                 google.maps.event.addListener(autocomplete, 'place_changed', function(){
-//                     var place = autocomplete.getPlace();
-//                     console.log("place:" + place);
-//                     element.value = place.name;
-//                      $scope.journey.source = place.name;
-//                      $scope.apply();
-//                 });
-//             }
-//         }
-//     });
 angular.module('howfary.directives', []).directive('googleMapsAddressField', function  () {
     return {
-
+        restrict: 'A',
+        scope: {
+            name: '='
+        },
+        link: function (scope, element, attrs){
+            var autocomplete = new google.maps.places.Autocomplete(element);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                element.value = place.name;
+                // scope.journey[element.name] = place.name;
+                // scope.apply();
+            });
+        }
     };
 });
