bb71488d620cc6001ba74195e5839a55e41ad967
Nanda Kishore <madhav.bnk@gmail.com>
2013-11-30 20:42:03 +0530

    Finally getting the directives working

--- howfaryweb/static/app.js
+++ howfaryweb/static/app.js
@@ -1,11 +1,3 @@
 "use strict";
 
 angular.module('howfary', ['howfary.controllers', 'howfary.directives']);
-
-function autocompleteFy (element) {
-    var autocomplete = new google.maps.places.Autocomplete(element);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        var place = autocomplete.getPlace();
-        element.value = place.name;
-    });
-}
--- howfaryweb/static/directives.js
+++ howfaryweb/static/directives.js
@@ -1,16 +1,14 @@
 angular.module('howfary.directives', []).directive('googleMapsAddressField', function  () {
     return {
+        require: 'ngModel',
         restrict: 'A',
-        scope: {
-            name: '='
-        },
-        link: function (scope, element, attrs){
-            var autocomplete = new google.maps.places.Autocomplete(element);
+        link: function ($scope, element, attrs){
+            var autocomplete = new google.maps.places.Autocomplete(element[0]);
             google.maps.event.addListener(autocomplete, 'place_changed', function () {
                 var place = autocomplete.getPlace();
-                element.value = place.name;
-                // scope.journey[element.name] = place.name;
-                // scope.apply();
+                $scope.journey[element[0].name] = [place.name,
+                                                   place.formatted_address].join(', ');
+                $scope.$apply();
             });
         }
     };
